7aacfe4ce0d8d4c60f4d8381313d50fbe7682a98
Abed <abed.ghaffer@gmail.com>
2022-10-04 16:21:57 +0200

    Insertion Sort

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Algorithms.Sorting;
+
+var random = new Random();
+var values = new int[1000];
+
+for (int i = 0; i < 1000; i++)
+    values[i] = random.Next(3400);
+
+// Insertion Sort
+var insertionSortResult = InsertionSort.Sort(values);
+//foreach (var val in values)
+//    Console.Write($"{val},");
+
---
+++ Algorithms/Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Sorting;
+
+using System.Diagnostics;
+
+internal static class InsertionSort
+{
+    public static int[] Sort(int[] unsortedValues)
+    {
+        var watch = Stopwatch.StartNew();
+        var sortedValues = unsortedValues;
+
+        for (int i = 0; i < unsortedValues.Length; i++)
+        {
+            var position = i;
+            var tempValue = unsortedValues[position];
+
+            while (position > 0 && unsortedValues[position - 1] > tempValue)
+            {
+                unsortedValues[position] = unsortedValues[position - 1];
+                position--;
+            }
+
+            unsortedValues[position] = tempValue;
+        }
+
+        watch.Stop();
+
+        Console.WriteLine($"It took {watch.Elapsed.TotalMilliseconds} seconds to finish {nameof(InsertionSort)}-{nameof(Sort)}");
+
+        return sortedValues;
+    }
+}
